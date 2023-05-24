6cb612ee8b18ec3833db81089a105a5ea86498df
OrigamiKing3612 <origamiking3612@gmail.com>
2023-05-24 13:09:16 -0500

    Added missing deaths

--- src/main/java/net/origamiking/games/oregontrail/gameplay/gameplay_features/Deaths.java
+++ src/main/java/net/origamiking/games/oregontrail/gameplay/gameplay_features/Deaths.java
@@ -8,4 +8,15 @@ public class Deaths {
         OregonTrailMain.println("You found a leak in your wagon. The wagon sinks....");
         End.end();
     }
+    public static void hunger() {
+        OregonTrailMain.println("Everyone collapses after not having any food.");
+        End.end();
+    }
+    public static void sleep_deprived() {
+        OregonTrailMain.println("Everyone is tired after not sleeping. They sit down to rest but never get back up. Everyone died of no sleep.");
+        End.end();
+    }
+    public static void hunger_and_sleep_deprived() {
+        OregonTrailMain.println("Everyone is so tired and hungry. They look for food in the wagon but find none so they lay down to get some sleep. They eventually die of hunger and sleep.");
+    }
 }
--- src/main/java/net/origamiking/games/oregontrail/utils/SubtractVariables.java
+++ src/main/java/net/origamiking/games/oregontrail/utils/SubtractVariables.java
@@ -1,13 +1,20 @@
 package net.origamiking.games.oregontrail.utils;
 
 import net.origamiking.games.oregontrail.OregonTrailMain;
+import net.origamiking.games.oregontrail.gameplay.gameplay_features.Deaths;
 import net.origamiking.games.oregontrail.variables.Variables;
 
 public class SubtractVariables {
     public static void time_1() {
         feed_people();
         if (Variables.FOOD_COUNTER <= 1 || Variables.REST_COUNTER <= 1) {
-            throw new RuntimeException("DIED OF HUNGER OR NO REST ADD A DEATH HERE IN SubtractVariables");
+            if (Variables.FOOD_COUNTER <= 1) {
+                Deaths.hunger();
+            } else if (Variables.REST_COUNTER <= 1) {
+                Deaths.sleep_deprived();
+            } else {
+                Deaths.hunger_and_sleep_deprived();
+            }
         } else {
             Variables.FOOD_COUNTER = Variables.FOOD_COUNTER - 1;
             Variables.REST_COUNTER = Variables.REST_COUNTER - 1;
@@ -18,8 +25,7 @@ public class SubtractVariables {
         if (Variables.POUNDS_OF_FOOD >= (Variables.RATIONS_PER_PERSON * Variables.PERSON_COUNTER)) {
             Variables.POUNDS_OF_FOOD = Variables.POUNDS_OF_FOOD - (Variables.RATIONS_PER_PERSON * Variables.PERSON_COUNTER);
         } else {
-            OregonTrailMain.println("YOU RAN OUT OF FOOD");
-            throw new RuntimeException("RAN OUT OF FOOD IN SubtractVariables, ADD A DEATH");
+            Deaths.hunger();
         }
     }
 }
