6740995b608b412ae04c4d9622716f77dfa8b39a
OrigamiKing3612 <origamiking3612@gmail.com>
2023-05-24 13:01:13 -0500

    added deaths

--- src/main/java/net/origamiking/games/oregontrail/gameplay/gameplay_features/Deaths.java
+++ src/main/java/net/origamiking/games/oregontrail/gameplay/gameplay_features/Deaths.java
@@ -1,7 +1,11 @@
 package net.origamiking.games.oregontrail.gameplay.gameplay_features;
 
+import net.origamiking.games.oregontrail.OregonTrailMain;
+import net.origamiking.games.oregontrail.utils.End;
+
 public class Deaths {
     public static void drowning_in_river() {
-        throw new RuntimeException("ADD DEATH IN Deaths for drowning in river");
+        OregonTrailMain.println("You found a leak in your wagon. The wagon sinks....");
+        End.end();
     }
 }
---
+++ src/main/java/net/origamiking/games/oregontrail/utils/End.java
@@ -0,0 +1,10 @@
+package net.origamiking.games.oregontrail.utils;
+
+import net.origamiking.games.oregontrail.OregonTrailMain;
+
+public class End {
+    public static void end() {
+        OregonTrailMain.println("You Died. Thanks for playing!");
+        System.exit(0);
+    }
+}
