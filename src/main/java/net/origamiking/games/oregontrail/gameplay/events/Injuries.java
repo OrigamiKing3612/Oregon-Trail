06257cb53c2773935ac339542db22cc55c1d7fa9
OrigamiKing3612 <origamiking3612@gmail.com>
2023-05-30 09:00:48 -0500

    Cleaned up some code

--- src/main/java/net/origamiking/games/oregontrail/gameplay/events/Injuries.java
+++ src/main/java/net/origamiking/games/oregontrail/gameplay/events/Injuries.java
@@ -1,7 +1,10 @@
 package net.origamiking.games.oregontrail.gameplay.events;
 
+import net.origamiking.games.oregontrail.variables.Variables;
+
 public class Injuries {
     public static void injuries() {
-        //TODO injuries
+//        TODO injuries
+        Variables.DAY_STUFF = "You have an uneventful day.";
     }
 }
