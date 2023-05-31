155a0a0633acc36e3e83f61ebfa6dff075174909
OrigamiKing3612 <origamiking3612@gmail.com>
2023-05-31 07:20:07 -0500

    Redid the End

--- src/main/java/net/origamiking/games/oregontrail/utils/End.java
+++ src/main/java/net/origamiking/games/oregontrail/utils/End.java
@@ -2,9 +2,20 @@ package net.origamiking.games.oregontrail.utils;
 
 import net.origamiking.games.oregontrail.OregonTrailMain;
 
+import javax.swing.*;
+
 public class End {
     public static void end() {
+
+        String[] options = {"Leave", "Replay"};
+
         OregonTrailMain.println("You Died. Thanks for playing!");
-        System.exit(0);
+
+        int choice = JOptionPane.showOptionDialog(null, "Choose an option:", "Replay or Leave", JOptionPane.DEFAULT_OPTION,
+                JOptionPane.PLAIN_MESSAGE, null, options, null);
+        switch (choice) {
+            case 0 -> System.exit(0);
+            case 1 -> OregonTrailMain.start();
+        }
     }
 }
